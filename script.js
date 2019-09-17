7bc8886baff956df5f10ab77e9fda278fd687e5f
emyvillegas <54033795+emyvillegas@users.noreply.github.com>
2019-09-17 18:22:39 -0500

    Validar Campos
    estoy en porceso de validar los campos, sin embargo aún no funcionan

--- script.js
+++ script.js
@@ -8,4 +8,18 @@ var contactos = [
     email: "juan.donoso@uia.mx", 
     telefono: "5555555555" }
 ];
-
+function añadir(){
+  document.getElementsByClassName("modal-open");
+}
+function validarcampos(){
+  var nombre = document.getElementById("nombre").value;
+  var email = document.getElementById("email").value;
+  var primera = validarnombre(nombre);
+  var segunda = validaremail(email);
+    if( nombre == null || nombre.length == 0 || /^\s+$/.test(nombre) ) {
+      return false;
+    } else{
+      document.getElementById("msg1").innerHTML="El campo Nombre no puede estár vacio";
+        document.getElementById("msg1").style.color="#CC0000";
+    }
+  }
