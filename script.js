16a3930fc5cd39d5f1f207572fd927acddcb09f0
macarenapoo <macarenapoo@gmail.com>
2019-09-25 10:06:27 -0500

    Mostrar todos los contactos del arreglo

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
 var contactos = [
-  { nombre: "Macarena Poo García", 
-    empresa: "Retail Zipline", 
-    email: "macarenapoo@gmail.com", 
-    telefono: "5556801216" },
-  { nombre: "Juan Fernando Donoso", 
-    empresa: "Universidad Iberoamericana", 
-    email: "juan.donoso@uia.mx", 
-    telefono: "5555555555" }
+  {
+    nombre: "Macarena Poo García",
+    empresa: "Retail Zipline",
+    email: "macarenapoo@gmail.com",
+    telefono: "5556801216"
+  },
+  {
+    nombre: "Juan Fernando Donoso",
+    empresa: "Universidad Iberoamericana",
+    email: "juan.donoso@uia.mx",
+    telefono: "5555555555"
+  }
 ];
 
+function mostrarContacto(contacto) {
+  // escribir adentro del ul el formato del li
+  var contenedorContactos = document.getElementById("contactos");
+  contenedorContactos.insertAdjacentHTML("beforeend",
+    `<li class="contacto">
+      <div class="actions">
+        <a><i class="fa fa-trash"></i></a>
+      </div>
+      <i class="userIcon fa fa-user"></i>
+      <h4 class="nombre">`+ contacto.nombre + `</h4>
+      <div class="datos">
+        <div class="dato">
+          <i class="fa fa-building"></i>
+          <span>`+ contacto.empresa + `</span>
+        </div>
+        <div class="dato">
+          <i class="fa fa-envelope"></i>
+          <a href="mailto:`+ contacto.email + `">` + contacto.email + `</a>
+        </div>
+        <div class="dato">
+          <i class="fa fa-phone"></i>
+          <a href="tel:`+ contacto.telefono + `">` + contacto.telefono + `</a>
+        </div>
+      </div>
+    </li>`);
+}
+
+function mostrarTodosLosContactos() {
+  contactos.forEach(mostrarContacto);
+}
+
+mostrarTodosLosContactos();
